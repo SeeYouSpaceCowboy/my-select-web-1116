b75e0a5a7ed682b025f08ce16737ee056e9347eb
Mohammed Chisti <mchisti@outlook.com>
2016-11-03 21:03:13 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  if block_given? && !collection.empty?
+    array = Array.new
+    collection.each { |element|
+	    if yield(element)
+	      array << element
+	    end
+    }
+    array
+  else
+    puts "You don't have a block!"
+  end
 end
